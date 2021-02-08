23edf82a810de1ca8d346bd91e9fc90812c1e8f9
TonaCV <mcv313@nyu.edu>
2021-02-08 23:15:43 +0400

    changes

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("hi")
+
+
+
+
+let image1 = document.getElementById('box1')
+
+image1.addEventListener('onmouseover', function(){
+  image.src = "pengif.gif"
+})
+
+image1.addEventListener('mouseout', function(){
+    image.src = "Pen.png"
+  })
