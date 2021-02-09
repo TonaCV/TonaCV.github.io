9ee442f5d107372ae54bbfef64b82b41d5470db5
TonaCV <mcv313@nyu.edu>
2021-02-09 17:59:17 +0400

    Changes

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-console.log("hi")
+
+let box1 = document.getElementById("box1")
+let box2 = document.getElementById("box2")
+let box3 = document.getElementById("box3")
+let box4 = document.getElementById("box4")
+let box5 = document.getElementById("box5")
 
 
-// let test = document.getElementById('pp');
-// let b1 = document.getElementById('box1')
-//
-//
-// b1.addEventListener("mouseenter", function( event ) {
-// event.target.style.background = 'url("pengif.gif")';
-// })
-//
-//
-// //
-// //
-// // gif1.addEventListener('mouseenter', function(event) {
+box1.addEventListener('click', () =>{
+    console.log("hi")
+    let vid = document.getElementById('vid');
+    vid.currentTime = 0;
+})
+
+box2.addEventListener('click', () =>{
+    console.log("hi")
+    let vid = document.getElementById('vid');
+    vid.currentTime = 14;
+})
+
+box3.addEventListener('click', () =>{
+    console.log("hi")
+    let vid = document.getElementById('vid');
+    vid.currentTime = 42;
+})
+
+box4.addEventListener('click', () =>{
+    console.log("hi")
+    let vid = document.getElementById('vid');
+    vid.currentTime = 53;
+})
+
+box5.addEventListener('click', () =>{
+    console.log("hi")
+    let vid = document.getElementById('vid');
+    vid.currentTime = 65;
+})
