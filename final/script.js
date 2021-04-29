87660e0edbbe54a2e2c4ac2d178a5a9c2f0d414a
TonaCV <mcv313@nyu.edu>
2021-04-29 13:10:48 +0400

    final

---
+++ final/script.js
@@ -0,0 +1,37 @@
+
+const flip1 = document.getElementById('content1_card');
+const flip2 = document.getElementById('content2_card');
+const flip3 = document.getElementById('content3_card');
+const flip4 = document.getElementById('content4_card');
+
+flip1.addEventListener('mouseenter', e => {
+  flip1.style.transform = 'rotateY(180deg)';
+});
+
+flip1.addEventListener('mouseleave', e => {
+  flip1.style.transform = 'rotateY(180deg)';
+});
+
+flip2.addEventListener('mouseenter', e => {
+  flip2.style.transform = 'rotateY(180deg)';
+});
+
+flip2.addEventListener('mouseleave', e => {
+  flip2.style.transform = 'rotateY(180deg)';
+});
+
+flip3.addEventListener('mouseenter', e => {
+  flip3.style.transform = 'rotateY(180deg)';
+});
+
+flip3.addEventListener('mouseleave', e => {
+  flip3.style.transform = 'rotateY(180deg)';
+});
+
+flip4.addEventListener('mouseenter', e => {
+  flip4.style.transform = 'rotateY(180deg)';
+});
+
+flip4.addEventListener('mouseleave', e => {
+  flip4.style.transform = 'rotateY(180deg)';
+});
