89d20c227c1bfcb84b21ff24ccfdf40701d7ddfe
TonaCV <mcv313@nyu.edu>
2021-05-02 15:17:44 +0400

    COMM

--- final/script.js
+++ final/script.js
@@ -40,3 +40,279 @@ parent4.addEventListener('mouseenter', e => {
 parent4.addEventListener('mouseleave', e => {
   flip4.style.transform = 'rotateY(0deg)';
 });
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+anime({
+  targets: '.cube1',
+  keyframes: [
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-80)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+  ],
+  rotateZ:360,
+  duration:5000,
+  loop:true,
+  direction:'alternate',
+  easing:'linear'
+});
+
+anime({
+  targets: '.cube2',
+  keyframes: [
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-80)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+  ],
+  rotateZ:360,
+  duration:5000,
+  loop:true,
+  direction:'alternate',
+  easing:'linear'
+});
+
+anime({
+  targets: '.cube3',
+  keyframes: [
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-80)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+  ],
+  rotateZ:360,
+  duration:5000,
+  loop:true,
+  direction:'alternate',
+  easing:'linear'
+});
+
+anime({
+  targets: '.cube4',
+  keyframes: [
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-80)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+  ],
+  rotateZ:360,
+  duration:5000,
+  loop:true,
+  direction:'alternate',
+  easing:'linear'
+});
+
+anime({
+  targets: '.cube5',
+  keyframes: [
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-80)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+  ],
+  rotateZ:360,
+  duration:5000,
+  loop:true,
+  direction:'alternate',
+  easing:'linear'
+});
+
+anime({
+  targets: '.cube6',
+  keyframes: [
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-80)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+  ],
+  rotateZ:360,
+  duration:5000,
+  loop:true,
+  direction:'alternate',
+  easing:'linear'
+});
+
+anime({
+  targets: '.cube7',
+  keyframes: [
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-80)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+  ],
+  rotateZ:360,
+  duration:5000,
+  loop:true,
+  direction:'alternate',
+  easing:'linear'
+});
+
+anime({
+  targets: '.cube8',
+  keyframes: [
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-80)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+  ],
+  rotateZ:360,
+  duration:5000,
+  loop:true,
+  direction:'alternate',
+  easing:'linear'
+});
+
+anime({
+  targets: '.cube9',
+  keyframes: [
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-80)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+  ],
+  rotateZ:360,
+  duration:5000,
+  loop:true,
+  direction:'alternate',
+  easing:'linear'
+});
+
+anime({
+  targets: '.cube10',
+  keyframes: [
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-80)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+  ],
+  rotateZ:360,
+  duration:5000,
+  loop:true,
+  direction:'alternate',
+  easing:'linear'
+});
+
+anime({
+  targets: '.cube11',
+  keyframes: [
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-80)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+  ],
+  rotateZ:360,
+  duration:5000,
+  loop:true,
+  direction:'alternate',
+  easing:'linear'
+});
+
+anime({
+  targets: '.cube12',
+  keyframes: [
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-80)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+  ],
+  rotateZ:360,
+  duration:5000,
+  loop:true,
+  direction:'alternate',
+  easing:'linear'
+});
+
+anime({
+  targets: '.cube13',
+  keyframes: [
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-80)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+  ],
+  rotateZ:360,
+  duration:5000,
+  loop:true,
+  direction:'alternate',
+  easing:'linear'
+});
+
+anime({
+  targets: '.cube14',
+  keyframes: [
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-80)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+  ],
+  rotateZ:360,
+  duration:5000,
+  loop:true,
+  direction:'alternate',
+  easing:'linear'
+});
+
+anime({
+  targets: '.cube15',
+  keyframes: [
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-80)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+  ],
+  rotateZ:360,
+  duration:5000,
+  loop:true,
+  direction:'alternate',
+  easing:'linear'
+});
+
+anime({
+  targets: '.cube16',
+  keyframes: [
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-80)},
+      {translateY: getRndInteger(-((screen.height/2)-100), (screen.height/2)-100)},
+      {translateX: getRndInteger(-((screen.width/2)-100), (screen.width/2)-100)},
+  ],
+  rotateZ:360,
+  duration:5000,
+  loop:true,
+  direction:'alternate',
+  easing:'linear'
+});
